3c6062703305840b3661bdafacd79228585a052b
Martín G <53666932+martinge17@users.noreply.github.com>
2024-07-26 16:57:21 +0200

    TMP increased reconnection retries

--- src/connection.rs
+++ src/connection.rs
@@ -1,63 +1,77 @@
-use btleplug::platform::{Peripheral};
-use btleplug::api::{Peripheral as _};
 use anyhow::Result;
-use tokio::time;
+use btleplug::api::Peripheral as _;
+use btleplug::platform::Peripheral;
 use std::time::Duration;
+use tokio::time;
 
-const NUM_RETRIES: i32 = 5;
+/*
+ TODO: TEMPORALLY DONT IMPLEMENT BACKOFF, ONLY TRY TO CONNECT INFINITELY (real limit to: 43200 (12 hours))
+*/
+
+const NUM_RETRIES: i32 = 43200;
 
 pub struct ConnectionHelper {
-  device: Peripheral
+    device: Peripheral,
 }
 
 impl ConnectionHelper {
-  pub fn new(device: &Peripheral) -> Self {
-    Self { device: device.clone() }
-  }
-
-  pub async fn connect(&self) -> Result<bool, btleplug::Error> { // Connect to Bluetooth device 
-    tracing::debug!("Connecting to device.");
-    let mut retries = NUM_RETRIES;
-    while retries >= 0 {
-      if self.device.is_connected().await? {
-        tracing::debug!("Connected to device");
-        break;
-      }
-      match self.device.connect().await {
-        Ok(_) => break,
-        Err(err) if retries > 0 => {
-          retries -= 1;
-          tracing::debug!("Retrying connection: {} retries left, reason: {}", retries, err);
-          time::sleep(Duration::from_secs(1)).await; //TODO: Exponential Backoff
-        },
-
-        Err(err) => return Err(err)
-      }
+    pub fn new(device: &Peripheral) -> Self {
+        Self {
+            device: device.clone(),
+        }
     }
 
-    Ok(true)
-  }
+    /*
+     TODO: TEMPORALLY DONT IMPLEMENT BACKOFF, ONLY TRY TO CONNECT INFINITELY
+    */
+    pub async fn connect(&self) -> Result<bool, btleplug::Error> {
+        // Connect to Bluetooth device
+        tracing::debug!("Connecting to device.");
+        let mut retries = NUM_RETRIES;
+        while retries >= 0 {
+            if self.device.is_connected().await? {
+                tracing::debug!("Connected to device");
+                break;
+            }
+            match self.device.connect().await {
+                Ok(_) => break,
+                Err(err) if retries > 0 => {
+                    retries -= 1;
+                    tracing::debug!(
+                        "Retrying connection: {} retries left, reason: {}",
+                        retries,
+                        err
+                    );
+                    time::sleep(Duration::from_secs(1)).await; //TODO: Exponential Backoff
+                }
 
-  pub async fn disconnect(&self) -> Result<bool> {
-    if !self.device.is_connected().await? {
-      tracing::debug!("Already disconnected.");
-      return Ok(true);
+                Err(err) => return Err(err),
+            }
+        }
+
+        Ok(true)
     }
 
-    if let Err(error) = self.device.disconnect().await {
-      tracing::error!("Could not disconnect: {}", error);
-      return Ok(false)
+    pub async fn disconnect(&self) -> Result<bool> {
+        if !self.device.is_connected().await? {
+            tracing::debug!("Already disconnected.");
+            return Ok(true);
+        }
+
+        if let Err(error) = self.device.disconnect().await {
+            tracing::error!("Could not disconnect: {}", error);
+            return Ok(false);
+        }
+
+        tracing::debug!("Disconnected from device");
+        Ok(true)
     }
 
-    tracing::debug!("Disconnected from device");
-    Ok(true)
-  }
-
-  pub async fn reconnect(&self) -> Result<bool> {
-    tracing::debug!("Reconnecting...");
-    self.disconnect().await?;
-    time::sleep(Duration::from_secs(5)).await;
-    self.connect().await?;
-    Ok(true)
-  }
+    pub async fn reconnect(&self) -> Result<bool> {
+        tracing::debug!("Reconnecting...");
+        self.disconnect().await?;
+        time::sleep(Duration::from_secs(5)).await;
+        self.connect().await?;
+        Ok(true)
+    }
 }
