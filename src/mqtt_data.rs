d5a567bb750dab217ac0b67daf0493f27627c0d4
Martín G <53666932+martinge17@users.noreply.github.com>
2024-07-27 13:06:02 +0200

    Migrated to MQTTv5 and implemented reconnection at first connect

--- src/mqtt_data.rs
+++ src/mqtt_data.rs
@@ -3,7 +3,7 @@ use anyhow::{anyhow, Error, Result};
 use paho_mqtt::AsyncClient;
 use paho_mqtt::{ConnectOptionsBuilder, CreateOptionsBuilder};
 use std::time::Duration;
-use tracing::error;
+use tracing::{error, info};
 
 pub struct MqttClient {
     pub client: AsyncClient,
@@ -14,29 +14,41 @@ impl MqttClient {
         let create_opts = CreateOptionsBuilder::new()
             .server_uri(&CONFIG.mqtt.broker)
             .client_id(&CONFIG.mqtt.client)
+            .mqtt_version(5) // Use MQTTv5
             .finalize();
 
         let mqtt_client = AsyncClient::new(create_opts).unwrap_or_else(|err| {
             panic!("Error creating the MQTT client: {:?}", err);
         });
 
-        let conn_opts = ConnectOptionsBuilder::new()
+        let conn_opts = ConnectOptionsBuilder::new_v5()
             .keep_alive_interval(Duration::from_secs(CONFIG.mqtt.keep_alive))
-            .clean_session(true)
             .automatic_reconnect(
                 Duration::from_secs(CONFIG.mqtt.reconnect_min),
                 Duration::from_secs(CONFIG.mqtt.reconnect_max),
             )
+            .retry_interval(Duration::from_secs(3))
+            .clean_start(true)
             .finalize();
 
-        tracing::info!("Starting MQTT connection");
+        info!("Starting MQTT connection");
 
         if let Err(e) = mqtt_client.connect(conn_opts).await {
             error!("Unable to connect to MQTT broker: {:?}", e);
-            return Err(anyhow!("Unable to connect to MQTT broker: {:?}", e));
+
+            while !mqtt_client.is_connected() {
+                info!("Reconnecting!");
+                match mqtt_client.reconnect().await {
+                    Ok(_) => break,
+                    Err(e) => {
+                        error!("Unable to connect to MQTT broker: {:?}", e);
+                        tokio::time::sleep(Duration::from_secs(3)).await;
+                    }
+                }
+            }
         }
 
-        tracing::info!("Connected to MQTT broker!");
+        info!("Connected to MQTT broker!");
 
         Ok(MqttClient {
             client: mqtt_client,
