7a9193b9f791a8880dfa9b8bf594a4f788d65057
Martín G <53666932+martinge17@users.noreply.github.com>
2024-08-13 22:38:02 +0200

    Added Initial API methods

---
+++ server/api/app/main.py
@@ -0,0 +1,179 @@
+from fastapi import FastAPI, Depends, HTTPException, Query
+from enum import Enum
+from typing import Optional
+from datetime import datetime
+from sqlalchemy.orm import Session
+from models import GeneralInfo as GeneralInfoModel  # SQLAlchemy model
+from models import BatteryInfo as BatteryInfoModel  # SQLAlchemy model
+from models import LocationInfo as LocationInfoModel  # SQLAlchemy model
+
+from schemas import GeneralInfo, BatteryInfo, LocationInfo  # Pydantic model
+
+
+from sqlalchemy import select
+
+from fastapi_pagination.ext.sqlalchemy import paginate
+from fastapi_pagination.links import Page
+from fastapi_pagination import add_pagination
+
+from database import SessionLocal, engine
+import models
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title="Electric Scooter information and control",
+    description="ChangeME",
+    version="0.1.0",
+)
+
+# Add pagination to the FastAPI app
+add_pagination(app)
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db = get_db()
+
+
+class RelayPowerModes(str, Enum):
+    open = "open"
+    closed = "closed"
+
+
+def validate_timeintervals(
+    start: Optional[datetime] = None, end: Optional[datetime] = None
+):
+    """Validates that start_time is not ahead of end_time"""
+    if (start != None and end != None) and start > end:
+        raise HTTPException(
+            status_code=400, detail="Start_date can't be posterior to end"
+        )
+
+
+@app.get(
+    "/api/v1/data",
+    summary="Retrieve the latest data for all categories (battery,location,general)",
+)
+async def global_data(
+    start_time: Optional[datetime] = None,
+    end_time: Optional[datetime] = None,
+    offset: int = 0,
+    limit: int = 10,
+):  # TODO
+    return {"Here you will see the latest available data"}
+
+
+# Datatime accepts timestamp with hours minutes and second. If you don't specify hours and minutes it defaults to 00:00 of
+# that date so if you type end_time 2024-08-01 it will show all results until 2024-08-01 00:00 so no result from that day will be shown
+# If you want to include per example the whole day 2024-08-01 in the query you should add 2024-08-01T23:59:59 or 2024-08-02
+# TODO: EXPLAIN THIS WITH EXAMPLE IN THE OPENAPI DOCS
+
+# Extra parameter to select asc or desc
+# If not time interval is specified, it will return the latest that starting from the most recent sample
+# If
+
+
+class OrderSelector(str, Enum):
+    asc = "asc"
+    desc = "desc"
+
+
+@app.get("/api/v1/data/general")
+async def general_data(
+    db: Session = Depends(get_db),
+    start_time: Optional[datetime] = None,
+    end_time: Optional[datetime] = None,
+    order: OrderSelector = "asc",
+) -> Page[GeneralInfo]:
+
+    # Check date errors before anything
+    validate_timeintervals(start_time, end_time)
+
+    # Construct the base query with optional filters
+    query = select(GeneralInfoModel)
+
+    if start_time:  # If there is start_time filter by that
+        query = query.where(GeneralInfoModel.time >= start_time)
+
+    if end_time:  # If there is end_time filter by that
+        query = query.where(GeneralInfoModel.time <= end_time)
+
+    # Order selector
+    if order == "asc":
+        query = query.order_by(GeneralInfoModel.time.asc())
+    else:
+        query = query.order_by(GeneralInfoModel.time.desc())
+
+    return paginate(db, query)
+
+
+@app.get("/api/v1/data/battery")
+async def battery_data(
+    db: Session = Depends(get_db),
+    start_time: Optional[datetime] = None,
+    end_time: Optional[datetime] = None,
+    order: OrderSelector = "asc",
+) -> Page[BatteryInfo]:
+
+    # Check date errors before anything
+    validate_timeintervals(start_time, end_time)
+
+    # Construct the base query with optional filters
+    query = select(BatteryInfoModel)
+
+    if start_time:  # If there is start_time filter by that
+        query = query.where(BatteryInfoModel.time >= start_time)
+
+    if end_time:  # If there is end_time filter by that
+        query = query.where(BatteryInfoModel.time <= end_time)
+
+    # Order selector
+    if order == "asc":
+        query = query.order_by(BatteryInfoModel.time.asc())
+    else:
+        query = query.order_by(BatteryInfoModel.time.desc())
+
+    return paginate(db, query)
+
+
+@app.get("/api/v1/data/location")
+async def location_data(
+    db: Session = Depends(get_db),
+    start_time: Optional[datetime] = None,
+    end_time: Optional[datetime] = None,
+    order: OrderSelector = "asc",
+) -> Page[LocationInfo]:  # TODO DOES NOT WORK WELL, MAKE IT RETURN A GeoJSON
+    # Check date errors before anything
+    validate_timeintervals(start_time, end_time)
+
+    # Construct the base query with optional filters
+    query = select(LocationInfoModel)
+
+    if start_time:  # If there is start_time filter by that
+        query = query.where(LocationInfoModel.time >= start_time)
+
+    if end_time:  # If there is end_time filter by that
+        query = query.where(LocationInfoModel.time <= end_time)
+
+    # Order selector
+    if order == "asc":
+        query = query.order_by(LocationInfoModel.time.asc())
+    else:
+        query = query.order_by(LocationInfoModel.time.desc())
+
+    return paginate(db, query)
+
+
+@app.post(
+    "/api/v1/command/set_power", summary="Sets the power relay state to open or closed"
+)
+async def set_power(mode: RelayPowerModes):  # TODO
+    return {"OPERATION X DONE. CURRENT STATE IS X"}
