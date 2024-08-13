8ee2804636584bb67e53c8269ff56f23f4988456
Martín G <53666932+martinge17@users.noreply.github.com>
2024-08-13 22:37:41 +0200

    Added SQLAlchemy models

---
+++ server/api/app/models.py
@@ -0,0 +1,52 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    TIMESTAMP,
+    DECIMAL,
+    SmallInteger,
+)
+from database import Base
+from geoalchemy2 import Geography
+
+
+# Since this API is intended to be read-only, constraints are not defined here.
+
+
+# Model the database tables for the ORM to access the DB.
+class GeneralInfo(Base):
+    __tablename__ = "general_info"
+
+    time = Column(TIMESTAMP(timezone=True), primary_key=True, nullable=False)
+    speed_kmh = Column(DECIMAL(4, 2), nullable=False)
+    trip_distance_m = Column(Integer, nullable=False)
+    uptime_sec = Column(Integer, nullable=False)
+    total_distance_m = Column(Integer, nullable=False)
+    est_distance_left_km = Column(DECIMAL(5, 2), nullable=False)
+    frame_temp = Column(DECIMAL(4, 2), nullable=False)
+
+
+class BatteryInfo(Base):
+    __tablename__ = "battery_info"
+
+    time = Column(TIMESTAMP(timezone=True), primary_key=True, nullable=False)
+    capacity = Column(SmallInteger, nullable=False)
+    percent = Column(SmallInteger, nullable=False)
+    voltage = Column(DECIMAL(4, 2), nullable=False)
+    current = Column(DECIMAL(5, 3), nullable=False)
+    power = Column(DECIMAL(5, 2), nullable=False)  # Generated column in DB
+    temp1 = Column(SmallInteger, nullable=False)
+    temp2 = Column(SmallInteger, nullable=False)
+
+
+class LocationInfo(Base):
+    __tablename__ = "location_info"
+
+    time = Column(TIMESTAMP(timezone=True), primary_key=True, nullable=False)
+    location = Column(
+        Geography(geometry_type="POINT", srid=4326), nullable=False
+    )  # TODO USE GeoAlchemy2 https://geoalchemy-2.readthedocs.io/en/latest/index.html AND CONVERT THIS INTO LATITUDE AND LONGITUDE FIELD FOR THE API
+    altitude = Column(DECIMAL(6, 2), nullable=False)
+    gps_speed = Column(DECIMAL(4, 2), nullable=False)
+
+
+# Model data classes
