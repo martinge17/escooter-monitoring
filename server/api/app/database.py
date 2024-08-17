b2079da84c6767ba7fda79e16de463f3b0f4c542
Martín G <53666932+martinge17@users.noreply.github.com>
2024-08-17 11:19:21 +0200

    Read database config from config.toml

--- server/api/app/database.py
+++ server/api/app/database.py
@@ -1,37 +1,30 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import sys
+from functools import lru_cache
 
-# import tomllib
-import logging
+from config import Settings, Database
 
-"""
-# Load configuration from file
-try:
-    file = open("config.toml", "rb")
-except Exception:
-    logging.error("Can't open config file", exc_info=True)
-    exit(1)
-else:
-    with file:
-        config = tomllib.load(file)
 
-template = "postgresql+psycopg://{user}:{password}@{hostname}:{port}/{dbname}"
-connection_str = template.format(
-    user=config["database"]["user"],
-    password=config["database"]["password"],
-    hostname=config["database"]["db_hostname"],
-    port=config["database"]["db_port"],
-    dbname=config["database"]["db_name"],
-)
-"""
+@lru_cache
+def get_db_settings():
+    return Settings().database
 
-connection_str = "postgresql+psycopg://api:changeme@localhost:5432/scooter_data"  # TODO: FOR DEV ONLY
 
-SQLALCHEMY_DB_URL = connection_str
+def connection_str(db_settings: Database):
 
-engine = create_engine(SQLALCHEMY_DB_URL)
+    template = "postgresql+psycopg://{user}:{password}@{hostname}:{port}/{dbname}"
+    connection_str = template.format(
+        user=db_settings.user,
+        password=db_settings.password,
+        hostname=db_settings.db_hostname,
+        port=db_settings.db_port,
+        dbname=db_settings.db_name,
+    )
+    return connection_str
+
+
+engine = create_engine(connection_str(get_db_settings()))
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
