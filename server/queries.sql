fabe09a12a08e643a02c194a5e48dfe6d7eb1959
Martín G <53666932+martinge17@users.noreply.github.com>
2024-08-18 12:47:50 +0200

    Added more queries

--- server/queries.sql
+++ server/queries.sql
@@ -16,7 +16,7 @@ SELECT trip_distance_m FROM general_info WHERE time >= NOW() - INTERVAL '10 seco
 
 SELECT uptime_sec FROM general_info WHERE time >= NOW() - INTERVAL '10 seconds' ORDER BY time DESC LIMIT 1
 
--- Odometer
+-- Odometer (total distance)
 
 SELECT total_distance_m FROM general_info ORDER BY time DESC LIMIT 1
 
@@ -24,9 +24,13 @@ SELECT total_distance_m FROM general_info ORDER BY time DESC LIMIT 1
 
 SELECT est_distance_left_km FROM general_info ORDER BY time DESC LIMIT 1
 
+-- Frame temperature
 
+SELECT frame_temp FROM general_info WHERE time >= NOW() - INTERVAL '10 seconds' ORDER BY time DESC LIMIT 1
 
+--- frame temp Hist
 
+SELECT time,frame_temp from general_info WHERE $__timeFilter(time)
 
 ----------- BATTERY ----------
 
@@ -34,13 +38,53 @@ SELECT est_distance_left_km FROM general_info ORDER BY time DESC LIMIT 1
 
 SELECT capacity FROM battery_info ORDER BY time DESC LIMIT 1
 
+--Battery capacity hist
+
+SELECT time,capacity from battery_info WHERE $__timeFilter(time)
+
 -- Latest battery percent
 
 SELECT capacity FROM battery_info ORDER BY time DESC LIMIT 1
 
--- Battery Histo
+-- Battery Percent Histo
 SELECT time,percent from battery_info WHERE $__timeFilter(time)
 
+-- Live battery voltage
+
+SELECT voltage FROM battery_info WHERE time >= NOW() - INTERVAL '10 seconds' ORDER BY time DESC LIMIT 1
+
+-- Battery Voltage Histo
+SELECT time,voltage from battery_info WHERE $__timeFilter(time)
+
+
+-- Live battery current
+
+SELECT current FROM battery_info WHERE time >= NOW() - INTERVAL '10 seconds' ORDER BY time DESC LIMIT 1
+
+-- Battery Voltage Histo
+SELECT time,current from battery_info WHERE $__timeFilter(time)
+
+
+-- Live battery power output current
+
+SELECT power FROM battery_info WHERE time >= NOW() - INTERVAL '10 seconds' ORDER BY time DESC LIMIT 1
+
+-- Battery Power output Histo
+SELECT time,power from battery_info WHERE $__timeFilter(time)
+
+-- Live battery temp1
+
+SELECT temp1 FROM battery_info WHERE time >= NOW() - INTERVAL '10 seconds' ORDER BY time DESC LIMIT 1
+
+-- Battery temp1 Histo
+SELECT time,temp1 from battery_info WHERE $__timeFilter(time)
+
+-- Live battery temp2
+
+SELECT temp2 FROM battery_info WHERE time >= NOW() - INTERVAL '10 seconds' ORDER BY time DESC LIMIT 1
+
+-- Battery temp2 Histo
+SELECT time,temp2 from battery_info WHERE $__timeFilter(time)
 
 ---- LOCATION -----
 
@@ -48,3 +92,17 @@ SELECT time,ST_X(ST_AsEWKT(location)) AS longitude,
        ST_Y(ST_AsEWKT(location)) AS latitude
 FROM location_info
 WHERE (ST_X(ST_AsEWKT(location)) <> 0) AND (ST_Y(ST_AsEWKT(location)) <> 0)
+
+-- Live altitude
+
+SELECT altitude FROM location_info WHERE time >= NOW() - INTERVAL '10 seconds' ORDER BY time DESC LIMIT 1
+
+-- Altitude Histo
+SELECT time,altitude from location_info WHERE $__timeFilter(time)
+
+-- Live gps speed
+
+SELECT gps_speed FROM location_info WHERE time >= NOW() - INTERVAL '10 seconds' ORDER BY time DESC LIMIT 1
+
+-- gps_speed Histo
+SELECT time,gps_speed from location_info WHERE $__timeFilter(time)
